70b2c022241d786b18bc45c5e12fb9553bdef122
FaresAriandha <AkunKoding2009@gmail.com>
2024-07-28 15:58:28 +0700

    Fixing CORS

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ origins = [
     "http://localhost",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://klasrumah.69dev.id/",
+    "https://klasrumah.69dev.id",
 ]
 
 app.add_middleware(
